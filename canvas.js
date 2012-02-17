ccd9f52da8a589cd7d2b468aad690f141a1fdd63
よや <yoya@awm.jp>
2012-02-18 04:05:20 +0900

    constructor and clear function

--- canvas.js
+++ canvas.js
@@ -1,3 +1,9 @@
-var SWFCanvas = function() {
-    ;
+var SWFCanvas = function(canvas_id) {
+    var canvas = document.getElementById(canvas_id);
+    this.width  = canvas.width;
+    this.height = canvas.height;
+    var ctx = canvas.getContext('2d');
+    this.clear = function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
 }
