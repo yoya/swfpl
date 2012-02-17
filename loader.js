7d28cc6ad0a52b16bf84bb7debd3cfd4462b7642
よや <yoya@awm.jp>
2012-02-18 04:05:48 +0900

    XMLHttpRequest

--- loader.js
+++ loader.js
@@ -1,3 +1,10 @@
 var SWFLoader = function(url, chara, object) {
-    ;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        ;
+    }
+    request.open('GET', url);
+    // http://javascript.g.hatena.ne.jp/edvakf/20100607/1275931930
+    request.overrideMimeType('text/plain; charset=x-user-defined');
+    request.send(null);
 }
