bdbba49ba5ee43e870d8baa483bcc6d4768df735
よや <yoya@awm.jp>
2012-02-25 03:58:29 +0900

    - display list variable. - control tags loop

--- object.js
+++ object.js
@@ -3,19 +3,33 @@ var SWFObject = function() {
     var controlTags = []
     var controlTags_idx = 0;
     var currentFrame = 0;
+    var displayDepthList = [];
+    var displayTable = {};
     this.appendTag = function(tag) {
         controlTags.push(tag);
     }
     this.playTick = function(player, chara, canvas, action, event) {
         console.debug("SWFObject::playTick");
+        var done = false;
         do {
             var tag = controlTags[controlTags_idx++];
             switch (tag.tag_code) {
-            case 1:
+            case 0: // End
+                done = true;
+                break;
+            case 1: // ShowFramen
+                break;
+            case 26: // PlaceObject2
+                break;
             default:
                 console.log(tag);
             }
         } while (tag.tag_code > 1);
-        currentFrame++;
+        if (done) {
+            currentFrame = 0;
+            controlTags_idx = 0;
+        } else {
+            currentFrame++;
+        }
     }
 }
