cec09de4e5cd9bff203265063c01ee3996b6dc4e
よや <yoya@awm.jp>
2012-02-25 03:33:32 +0900

    move jpeg procedure from chara.js to image.js

--- chara.js
+++ chara.js
@@ -7,63 +7,14 @@ var SWFChara = function() {
     this.appendJpegTableTag = function(tag) {
         jpegTables = tag.JPEGData;
     }
-    var SOI  = 0xFFD8;
-    var SOF0 = 0xFFC0;
-    var SOF2 = 0xFFC2;
-    var DQT  = 0xFFDB;
-    var DHT  = 0xFFC4;
-    var SOS  = 0xFFDA;
-    var EOI  = 0xFFD9;
     this.appendJpegTag = function(tag) {
         console.debug("SWFChara::appendJpegTag");
-        var img = new Image();
-        bitio = new BitIO();
-        bitio.input(tag.ImageData);
-        var marker;
-        var dqt = '', dht = '';
-        while (marker = bitio.getUI16BE(2)) {
-            switch (marker) {
-              case SOI:
-              case EOI:
-                break;
-              default:
-                var len = bitio.getUI16BE(2);
-                bitio.incrementOffset(len - 2, 0);
-                break;
-              case SOF0:
-              case SOF2:
-                var len = bitio.getUI16BE(2);
-                bitio.incrementOffset(-4, 0);
-                var sof = bitio.getData(len + 2);
-                break;
-              case DQT:
-                var len = bitio.getUI16BE(2);
-                bitio.incrementOffset(-4, 0);
-                dqt += bitio.getData(len + 2);
-                break;
-              case DHT:
-                var len = bitio.getUI16BE(2);
-                bitio.incrementOffset(-4, 0);
-                dht += bitio.getData(len + 2);
-                break;
-              case SOS:
-                bitio.incrementOffset(-2, 0);
-                var sos_eoi = bitio.getDataUntil(null);
-                break;
-            }
-
-        }
-        if (typeof dqt === '') {
-            dqt_dht = jpegTables;
-        } else {
-            dqt_dht = dqt + dht;
-        }
-        var jpegData = "\xFF\xD8" + sof + dqt_dht + sos_eoi;
-        img.src = "data:image/jpeg;base64,"+base64encode(jpegData);
+        var img = SWFJpeg(imagedata, jpegtables);
         characterData[tag.CharacterID] = {image:img};
     }
     this.appendLosslessTag = function(tag) {
         var img = new Image();
+        
         characterData[tag.CharacterID] = {image:img};
     }
     this.appendShapeTag = function(tag) {
--- image.js
+++ image.js
@@ -1,6 +1,50 @@
+
 function SWFJpeg(imagedata, jpegtables) {
-    image = null;
-    return image;
+    var img = new Image();
+    var bitio = new BitIO();
+    bitio.input(tag.ImageData);
+    var marker;
+    var dqt = '', dht = '';
+    while (marker = bitio.getUI16BE(2)) {
+        switch (marker) {
+        case 0xFFD8: // SOI
+        case 0xFFD9: // EOI
+            break;
+        default:
+            var len = bitio.getUI16BE(2);
+            bitio.incrementOffset(len - 2, 0);
+            break;
+        case 0xFFC0: // SOF0
+        case 0xFFC2: // SOF2
+            var len = bitio.getUI16BE(2);
+            bitio.incrementOffset(-4, 0);
+            var sof = bitio.getData(len + 2);
+            break;
+        case 0xFFDB: // DQT
+            var len = bitio.getUI16BE(2);
+            bitio.incrementOffset(-4, 0);
+            dqt += bitio.getData(len + 2);
+            break;
+        case 0xFFC4: // DHT
+            var len = bitio.getUI16BE(2);
+            bitio.incrementOffset(-4, 0);
+            dht += bitio.getData(len + 2);
+            break;
+        case 0xFFDA: // SOS
+            bitio.incrementOffset(-2, 0);
+            var sos_eoi = bitio.getDataUntil(null);
+            break;
+        }
+        
+    }
+    if (typeof dqt === '') {
+        dqt_dht = jpegTables;
+    } else {
+        dqt_dht = dqt + dht;
+    }
+    var jpegData = "\xFF\xD8" + sof + dqt_dht + sos_eoi;
+    img.src = "data:image/jpeg;base64,"+base64encode(jpegData);
+    return img;
 }
 
 
