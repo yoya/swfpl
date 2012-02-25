87eb71d2ea55b267b81b2550714b05bd0afbc6d7
よや <yoya@awm.jp>
2012-02-25 11:39:42 +0900

    ShowFrame, PlaceObject, RemoveObject

--- object.js
+++ object.js
@@ -3,10 +3,29 @@ var SWFObject = function() {
     var controlTags = []
     var controlTags_idx = 0;
     var currentFrame = 0;
-    var displayDepthList = [];
-    var displayTable = {};
+    var displayListTable = {}; // depth => ...
     this.appendTag = function(tag) {
         controlTags.push(tag);
+    }
+    this.showFrame = function(chara, canvas) {
+        n = displayListTable.length;
+        if (typeof n === 'undefined') {
+            return ; // nothing to do
+        }
+        var depthList = new Array(n);
+        for (var depth in displayListTable) {
+            depthList[i++] = depth;
+        }
+        depthList = depthList.sort();
+        for (var i = 0, n = depthList.length ; i < n ; i++) {
+            var depth = depthList[i];
+            console.log('depth='+depth);
+            console.log(depthList);
+            var place = displayListTable[depth];
+            console.log(place);
+            var character = chara.getCharacter(place.CharacterId);
+            
+        }
     }
     this.playTick = function(player, chara, canvas, action, event) {
         console.debug("SWFObject::playTick");
@@ -18,8 +37,31 @@ var SWFObject = function() {
                 done = true;
                 break;
             case 1: // ShowFramen
+                this.showFrame(chara, canvas);
                 break;
             case 26: // PlaceObject2
+                if (tag.PlaceFlagHasMove) {
+                    if (tag.PlaceFlagHasCharaceter) {
+                        displayListTable[tag.Depth].CharacterId = tag.CharacterId;
+                    }
+                    if (tag.PlaceFlagHasMatrix) {
+                        displayListTable[tag.Depth].Matrix = tag.Matrix;
+                    }
+                    if (tag.PlaceFlagHasColorTransform) {
+                        displayListTable[tag.Depth].ColorTransform = tag.ColorTransform;
+                    }
+                    if (tag.PlaceFlagHasRatio) {
+                        displayListTable[tag.Depth].Ratio = tag.Ratio;
+                    }
+                    if (tag.PlaceFlagHasName) {
+                        displayListTable[tag.Depth].Name = tag.Name;
+                    }
+                } else {
+                    displayListTable[tag.Depth] = tag;
+                }
+                break;
+            case 28: // RemoveObject2
+                delete displayListTable[tag.Depth];
                 break;
             default:
                 console.log(tag);
