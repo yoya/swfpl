99a79834bb51287947aead9a34dc3c26f4e76377
Yoya <yoya@awm.jp>
2012-03-01 05:02:03 +0900

    convertLineEdges

--- vector.js
+++ vector.js
@@ -4,9 +4,41 @@
 // line: {width: color}
 
 var SWFVector = function(fillStyles, lineStyles, shapeRecords) {
-    var convertFillEdges = function(shapeRecords, startOffset, endOffset) {
-	console.debug("startOffset:"+startOffset+" endOffset:"+endOffset);
-	return [];
+    var convertFillEdges = function(shapeRecords, startOffset, endOffset, fillStyles) {
+	var fillEdgesList = []
+	console.debug("convertFillEdges: startOffset:"+startOffset+" endOffset:"+endOffset);
+	for (i = startOffset ; i <= endOffset ; i++) {
+	    var record = shapeRecords[i];
+	    ;
+	}
+	return fillEdgesList;
+    }
+    var convertLineEdges = function(shapeRecords, startOffset, endOffset, lineStyles) {
+	var lineEdgesList = []
+	//	console.debug("convertLineEdges: startOffset:"+startOffset+" endOffset:"+endOffset);
+	var lineEdges = [];
+	var currentLineStyle = 0;
+	var startOfShapeRecords = shapeRecords[startOffset]
+	if (startOfShapeRecords.LineStyle) {
+	    currentLineStyle = shapeRecords[startOffset].LineStyle;
+	}
+	for (i = startOffset + 1 ; i <= endOffset ; i++) {
+	    var record = shapeRecords[i];
+	    if (SWFSTYLECHANGERECORD || (i === endOffset)) {
+		if ((('LineStyle' in record) || 
+		     (currentLineStyle !== record.LineStyle)) ||
+		    (i === endOffset)) {
+		    if (currentLineStyle) {
+			lineEdgesList.push(lineStyles[currentLineStyle], lineEdges);
+		    }
+		    var lineEdges = [];
+		    currentLineStyle = record.LineStyle;
+		}
+	    } else {
+		lineEdges.push(record);
+	    }
+	}
+	return lineEdgesList;
     }
     console.debug("SWFVector");
     console.debug(fillStyles);
@@ -17,12 +49,10 @@ var SWFVector = function(fillStyles, lineStyles, shapeRecords) {
     var startOffset = 0;
     for (i = 1, n = shapeRecords.length ; i < n ; i++) {
 	var record = shapeRecords[i];
-	console.debug('record['+i+']');
-	console.debug(record);
-	if (record.moveX || (record instanceof SWFENDSHAPERECORD)) {
+	if (('MoveX' in record) || (i === (n - 1))) {
 	    endOffset = i - 1;
-	    edgesWithFillStyles = convertFillEdges(shapeRecords, startOffset, endOffset);
-	    //
+	    edgesWithFillStyles = convertFillEdges(shapeRecords, startOffset, endOffset, fillStyles);
+	    edgesWithLineStyles = convertLineEdges(shapeRecords, startOffset, endOffset, lineStyles);
 	    // next paths
 	    if (record.FillStyles) {
 		fillStyles = record.FillStyles.FillStyles;
