bd4b0de5eaa29253a466b839985dd005f6301833
Yoya <yoya@awm.jp>
2012-03-02 15:41:37 +0900

    - function skeltons: variable, property and doaction

--- action.js
+++ action.js
@@ -1,3 +1,28 @@
-var SWFAction = function() {
-    ;
+var SWFAction = function(object, parentAction) {
+    var variables = {};
+    this.setVariable = function(name, value)  {
+	variables[name] = value;
+    }
+    this.getVariable = function(name)  {
+	if (name in variables) {
+	    return variables[name];
+	} else {
+	    return null;
+	}
+    }
+    this.setProperty = function(num, value)  {
+        switch (num) {
+	default:
+	break;
+	}
+    }
+    this.getProperty = function(num)  {
+        switch (num) {
+	default:
+	break;
+	}
+    }
+    this.doAction = function(tag) {
+	;
+    }
 }
